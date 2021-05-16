fd47acb38df90b083f75f2e4a8d150fe8a3e968b
ALEX SANDRO <alexsandrojr.303@gmail>
2021-05-16 17:04:29 -0300

    add function to clear dates and add contents

--- index.js
+++ index.js
@@ -1,15 +1,35 @@
+preencherTabelaFilmes();
 
 const personagensContador = document.getElementById("personagens");
 const veiculosContador = document.getElementById("vehicles");
 const planetasContador = document.getElementById("planetas");
 const navesContador = document.getElementById("naves");
 
-preencherTabelaFilmes();
+
+const btnFilmes = document.getElementById("btnFilmes");
+const btnPesonagens = document.getElementById("btnPesonagens");
+const btnPlanetas = document.getElementById("btnPlanetas");
+const btnNaves = document.getElementById("btnNaves");
+
+
+btnFilmes.addEventListener('click', preencherTabelaFilmes);
+btnPesonagens.addEventListener('click', preencherPeople);
+btnPlanetas.addEventListener('click', preencherPlanets)
+btnNaves.addEventListener('click', preencherNaves)
+
 preencherContadores();
 desenharGrafico();
 
 google.charts.load('current', {'packages':['corechart']});
 google.charts.setOnLoadCallback(desenharGrafico);
+
+function clearDates(){
+    const tHead = $("#tableHead");
+    const tBody = $("#tableBody");
+    
+    tHead.empty();
+    tBody.empty();
+}
 
 async function desenharGrafico() {
     const response = await swapiGet('starships/');
@@ -33,7 +53,6 @@ async function desenharGrafico() {
   chart.draw(data, options);
 }
 
-
 function preencherContadores() {
     
     Promise.all([swapiGet('people/'), 
@@ -50,9 +69,11 @@ function preencherContadores() {
 }
 
     async function preencherTabelaFilmes() {
+        clearDates();
+
         const response = await swapiGet('films/');
         const tableData = response.data.results;
-        
+
         $('#tableHead').append(`
         <th>Titulo</th>
         <th>Data de Lançamento</th>
@@ -69,9 +90,83 @@ function preencherContadores() {
             <td>${film.episode_id}</td>
             </tr>`)
     });
+}
+
+    async function preencherPeople() {
+        clearDates()
+        const response = await swapiGet('people/');
+        const tableData = response.data.results;
+    
+        $('#tableHead').append(`
+        <th>Nome</th>
+        <th>Genero</th>
+        <th>Altura</th>
+        <th>Peso</th>
+    `) 
+        tableData.forEach(people => {
+
+        $('#tableBody').append(`
+            <tr>
+            <td>${people.name}</td>
+            <td>${people.gender}</td>
+            <td>${people.height}</td>
+            <td>${people.mass}</td>
+            </tr>`)
+    })
+       
 };   
+
+async function preencherPlanets() {
+    clearDates()
+    const response = await swapiGet('planets/');
+    const tableData = response.data.results;
+
+    $('#tableHead').append(`
+    <th>Nome</th>
+    <th>População</th>
+    <th>Clima</th>
+    <th>Terreno</th>
+`) 
+    tableData.forEach(planet => {
+
+    $('#tableBody').append(`
+        <tr>
+        <td>${planet.name}</td>
+        <td>${planet.population}</td>
+        <td>${planet.climate}</td>
+        <td>${planet.terrain}</td>
+        </tr>`)
+})
+   
+};
+
+async function preencherNaves() {
+
+    clearDates()
+    const response = await swapiGet('starships/');
+    const tableData = response.data.results;
+
+    $('#tableHead').append(`
+    <th>Nome</th>
+    <th>Modelo</th>
+    <th>Passageiros</th>
+    <th>Classe</th>
+`) 
+    tableData.forEach(starships => {
+
+    $('#tableBody').append(`
+        <tr>
+        <td>${starships.name}</td>
+        <td>${starships.model}</td>
+        <td>${starships.passengers}</td>
+        <td>${starships.starship_class}</td>
+        </tr>`)
+})
+   
+};      
+
 
 
 function swapiGet(param){
     return axios.get(`https://swapi.dev/api/${param}`);
-}
+};
